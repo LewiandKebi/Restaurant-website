4bf48aa975c9f080c21f551a6785b017b63942d6
LewiandKebi <etafe1123@gmail.com>
2026-03-31 06:36:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({
+        behavior: "smooth"
+      });
+  });
+});
+
+/* for view menu button at hero section */
+document.getElementById("menuBtn")
+.addEventListener("click", function() {
+  document.getElementById("menu")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+/* for the navbar */
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
